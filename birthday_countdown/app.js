bf02a5db0eb4b675b89eee502aa46df97d64443e
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-02-21 14:35:20 +0000

    Add files via upload

--- birthday_countdown/app.js
+++ birthday_countdown/app.js
@@ -1,5 +1,21 @@
 const timeLeft = document.getElementById("time-left")
 
-const today = new Date()
 
-console.log(today)
+const birthday = new Date("08/01/2024")
+const second =1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24 
+let timerId
+
+function countDown(){
+    const today = new Date()
+    const timespan = birthday - today
+
+    if (timespan <= 0) {
+        timeLeft.innerHTML ="Happy Birthday"
+        clearInterval(timerId)
+    }
+}
+
+timerId = setInterval(countDown,second)
