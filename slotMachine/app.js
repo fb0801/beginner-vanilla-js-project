00ca8d1e0af0ad4e60959707b14cdc07c0928300
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-05 18:43:09 +0000

    Add files via upload

--- slotMachine/app.js
+++ slotMachine/app.js
@@ -2,6 +2,25 @@
 
 const prompt =require("prompt-sync")();
 
+
+const ROWS = 3;
+const COLS =3;
+
+const SYMBOLS_COUNT= {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOLS_VALUES ={
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
 const deposit =() => {
     while (true){
     const depositAmount = prompt("Enter a desposit amount: ");
@@ -30,5 +49,42 @@ const getNumberOfLines =() =>{
     
 }
 
+const getBet =(balance, lines) => {
+    while (true){
+        const bet = prompt("Enter bet per line: ");
+        const numberBet =parseFloat(bet);
+    
+        if (isNaN(numberBet) || numberBet <=0 || numberBet >  balance / lines) {
+            console.log("Invalid bet, Try again");
+        } else {
+            return numberBet;
+        }
+    }
+};
+
+const spin =() => {
+    const symbols =[];
+    for ([symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for( let i=0;i<count; i++){
+            symbols.push(symbol);
+        }
+    }
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j =0; j < ROWS; j++){
+            const randomIndex = math.floor(math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+        return reels
+
+};
+const reels = spin()
 let balance = deposit();
 const NumberOfLines =getNumberOfLines();
+const bet = getBet(balance, NumberOfLines);
+
+
