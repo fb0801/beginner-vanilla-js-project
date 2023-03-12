343a5ce03accda868d9b41da855479cbbfc236a3
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-12 19:00:23 +0000

    Add files via upload

--- 2048/app.js
+++ 2048/app.js
@@ -13,6 +13,38 @@ document.addEventListener("DOMContentLoaded" ,() => {
             gridDisplay.appendChild(square)
             squares.push(square)
         }
+        generate()
+        generate()
     }
         createBoard()
+
+        //generate ran number
+
+        function generate (){
+            let randomNumber = math.floor(Math.random() * squares.length)
+            if (squares[randomNumber].innerHTML ==0) {
+                squares[randomNumber].innerHTML == 2
+
+            }else generate()
+        }
+
+        //swipe rght
+        function moveRight () {
+            for (let i=0; i < 16; i++) {
+                if (i %4 ===0) {
+                    let totalOne = squares[i].innerHTML
+                    let totalTwo = squares[i+1].innerHTML
+                    let totalThree = squares[i+2].innerHTML
+                    let totalFour = squares[i+3].innerHTML
+                    let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+
+                    let filteredRow = row.filter(num => num)
+                }
+            }
+        }
+
+        moveRight()
+
+
 })
