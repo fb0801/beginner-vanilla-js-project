c31859e0fa6bbe3bf84bf3dcca44afc53f19e22e
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-12 19:23:17 +0000

    Add files via upload

--- 2048/app.js
+++ 2048/app.js
@@ -53,7 +53,54 @@ document.addEventListener("DOMContentLoaded" ,() => {
             }
         }
 
-        moveRight()
+        //mve left
+        function moveLeft () {
+            for (let i=0; i < 16; i++) {
+                if (i %4 ===0) {
+                    let totalOne = squares[i].innerHTML
+                    let totalTwo = squares[i+1].innerHTML
+                    let totalThree = squares[i+2].innerHTML
+                    let totalFour = squares[i+3].innerHTML
+                    let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
 
+
+                    let filteredRow = row.filter(num => num)
+                    let missing = 4 - filteredRow.length
+                    let zeros = Array(missing).fill(0)
+                    let newRow = filteredRow.concat(zeros)
+
+                    squares[i].innerHTML = newRow[0]
+                    squares[i+1].innerHTML = newRow[1]
+                    squares[i+2].innerHTML = newRow[2]
+                    squares[i+3].innerHTML = newRow[3]
+
+                }
+            }
+        }
+
+      function combinedRow(){
+        for (let i =0; i<15 ; i++){
+            if (squares[i].innerHTML ===squares[i+1].innerHTML){
+                let combinedTotal =parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i].innerHTML = 0 
+            }
+        }
+      }
+
+      //keycodes
+
+      function control(e){
+        if (e.keycode === 39) {
+
+        }
+      }
+
+      function keyRight(){
+        moveRight()
+        combinedRow()
+        moveRight()
+        generate()
+      }
 
 })
