612068410fd16ed8f7cf978aced75a9647e919c8
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-26 19:58:43 +0100

    Add files via upload

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -65,5 +65,26 @@ function deleteCheck(e){
 
 function filterTodo(e){
 const todos = todoList.childNodes
+todos.foreEach(function(todo){
+switch(e.target.value){
+    case "all":
+        todo.style.display = 'flex'
+        break
+    case "completed":
+        if(todos.classList.contains('completed')){
+            todo.style.display= 'flex'
+        }else {
+            todo.style.display= 'none'
+        } 
+        break
+        case "uncompleted":
+            if(!todo.classList.contains('completed')){
+                todo.style.display = "flex"
+            } else {
+                todo.style.display= 'none'
+            }
+            break
 
+        }
+    })
 }
